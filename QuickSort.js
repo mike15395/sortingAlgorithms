301f9e48fdd0da905359d96e9ef0146ce161e849
mike15395 <mikhildesai@gmail.com>
2022-04-22 13:32:51 +0530

    quick sort completed

--- QuickSort.js
+++ QuickSort.js
@@ -1,4 +1,32 @@
 function partition(arr,start,end) {
-    let pivot = arr[end];
+    let pivot = arr[end]; //taking pivot as the last element of array
+    let smaller = start;
+    for (let i = start; i < end; i++){
+        if (arr[i] < pivot) {
+            swap(arr,i, smaller);
+            smaller++;
+        }
+    }
+    swap(arr,end,smaller);
     
+    
+    return smaller;
 }
+
+function swap(arr,a,b){
+    let temp=arr[b];
+    arr[b]= arr[a];
+    arr[a] = temp;
+}
+
+function quickSort(arr, start, end) {
+    if (start >= end) {
+        return;
+    }
+    let p = partition(arr, start, end);
+    quickSort(arr, start, p - 1)
+    quickSort(arr, p + 1, end);
+}
+let arr = [7,-2,4,1,6,5,0,-4,2]
+quickSort(arr, 0, arr.length - 1);
+console.log(arr);
